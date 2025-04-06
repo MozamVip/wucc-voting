4d84c2a1bf23b578900b0ee8393b6f231ff7ff46
MozamVip <84370388+MozamVip@users.noreply.github.com>
2025-04-06 14:25:48 +0100

    Update res.js

--- js/res.js
+++ js/res.js
@@ -83,7 +83,7 @@ const awardData = [
                 description: "Best Dressed Male in Computing",
                 type: "individual",
                 nominees: [
-                    { id: 701, name: "Ogatosin", votes: 15, image: "img/ogatosin.jpeg" },
+                    { id: 701, name: "Ogatosin", votes: 25, image: "img/ogatosin.jpeg" },
                     { id: 702, name: "Etinosa Frank", votes: 20, image: "img/Frank.jpeg" },
                     { id: 703, name: "Hassan Bello", votes: 0, image: "img/hassan.jpeg" },
                     { id: 704, name: "AbdulMalik", votes: 0, image: "img/Maleek.jpeg" }
