604fcc6257c57e6a5911b351b55a307227bf09fa
MozamVip <84370388+MozamVip@users.noreply.github.com>
2025-04-06 22:09:09 +0100

    Update res.js

--- js/res.js
+++ js/res.js
@@ -6,7 +6,7 @@ const awardData = [
                 type: "individual",
                 nominees: [
                     { id: 101, name: "Etinosa Frank", votes: 501, image: "img/Frank.jpeg" },
-                    { id: 102, name: "Sotonye Green", votes: 299, image: "img/green.jpeg" },
+                    { id: 102, name: "Sotonye Green", votes: 659, image: "img/green.jpeg" },
                     { id: 103, name: "Godswill Enajewhe", votes: 0, image: "img/Godswill.jpeg" },
                     { id: 104, name: "Nelson", votes: 0, image: "img/nelson.jpeg" },
                     { id: 105, name: "Uyioghosa", votes: 5, image: "img/uyi.jpeg" },
