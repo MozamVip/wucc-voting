b4d9f8cab0951ee39860b315538c6ac74e96b15a
MozamVip <84370388+MozamVip@users.noreply.github.com>
2025-04-23 23:13:01 +0100

    Update res.js

--- js/res.js
+++ js/res.js
@@ -49,7 +49,7 @@ const awardData = [
                 description: "Recognizing the most efficient Course rep of the year",
                 type: "individual",
                 nominees: [
-                    { id: 401, name: "Treasure", votes: 21, image: "img/treasure.jpeg" },
+                    { id: 401, name: "Treasure", votes: 71, image: "img/treasure.jpeg" },
                     { id: 402, name: "francess", votes: 20, image: "img/francess.jpeg" },
                     { id: 403, name: "JohnnyAI", votes: 0, image: "img/johnnyAI.jpeg" },
                     { id: 404, name: "Simon", votes: 0, image: "img/simon.jpeg" }
