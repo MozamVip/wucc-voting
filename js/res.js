a7c28743719cf27dd4dfe4d12b1097f96ff8c8e7
MozamVip <84370388+MozamVip@users.noreply.github.com>
2025-04-23 23:34:36 +0100

    Update res.js

--- js/res.js
+++ js/res.js
@@ -193,7 +193,7 @@ const awardData = [
                     { id: 1301, name: "Hamza", votes: 3, image: "img/hamza.jpeg" },
                     { id: 1302, name: "Double G", votes: 0, image: "img/doubleG.jpeg" },
                     { id: 1303, name: "Freddy", votes: 0, image: "img/freddy.jpeg" },
-                    { id: 1304, name: "Classic Clown", votes: 30, image: "img/clown.jpeg" },
+                    { id: 1304, name: "Classic Clown", votes: 130, image: "img/clown.jpeg" },
                     { id: 1305, name: "Fortune Oputa", votes: 36, image: "" }
                 ]
                 
